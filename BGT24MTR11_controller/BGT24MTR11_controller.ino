de8bad4a5517987bded5662a775c29da7401ce08
giocic2 <74670040+giocic2@users.noreply.github.com>
2021-02-23 13:25:01 +0100

    INO code update after DSO testing of SPI communication
    Oscilloscope settings added as comment in the end of the code. Minor changes like variable names.

--- BGT24MTR11_controller/BGT24MTR11_controller.ino
+++ BGT24MTR11_controller/BGT24MTR11_controller.ino
@@ -1,32 +1,36 @@
 #include <SPI.h>
 
 // SPI PinOut and Transmitted 16-bit Word
-const int cs = 10;    //chip select
-const int clk = 13;   //clock
-const int so = 11;    //serial out
+const int chipSelectPin = 10;    //chip select
+const int clockPin = 13;   //clock
+const int serialOutPin = 11;    //serial out
 
-const byte MSB = B00000000;     //LNA Gain Red off -          Not Used       -         Not Used       - disable TX pwr on -        AMUX2 0     -   Test Bit  -   Test Bit  -   AMUX1 0
-const byte LSB = B00001000;     //    AMUX0 0      - disable 64k divider off - disable 16 divider off - Hi LO buffer off  - High TX buffer off - TW pwr red2 - TW pwr red1 - TW pwr red0
+// const byte MSB = B00000000;     //LNA Gain Red off -          Not Used       -         Not Used       - disable TX pwr on -        AMUX2 0     -   Test Bit  -   Test Bit  -   AMUX1 0
+// const byte LSB = B00001000;     //    AMUX0 0      - disable 64k divider off - disable 16 divider off - Hi LO buffer off  - High TX buffer off - TW pwr red2 - TW pwr red1 - TW pwr red0
+
+const byte communication_MSB = B00001111;
+const byte communication_LSB = B11110000;
 
 void setup() {
 
-  pinMode(cs,OUTPUT);
-  pinMode(clk,OUTPUT);
-  pinMode(so,OUTPUT);
+  pinMode(chipSelectPin,OUTPUT);
+  pinMode(clockPin,OUTPUT);
+  pinMode(serialOutPin,OUTPUT);
 
-  Serial.begin(9600);
+  // Serial.begin(9600);
 
-  Serial.println("SPI communication started...");
-  SPI.beginTransaction(SPISettings(16000000,MSBFIRST,SPI_MODE1)); //16MHz
-  digitalWrite(cs,HIGH);
-  delay(100);
-  digitalWrite(cs,LOW);
-  delay(1);
-  SPI.transfer(MSB);
-  SPI.transfer(LSB);
-  delay(1);
-  digitalWrite(cs,HIGH);
-  delay(100);
+  // Serial.println("SPI communication started...");
+  SPI.beginTransaction(SPISettings(125000,MSBFIRST,SPI_MODE0)); // min. 125kHz; strange signals on clockPin line due to this line.
+  delay(1000);
+  digitalWrite(chipSelectPin,HIGH);
+  delay(0.2);
+  digitalWrite(chipSelectPin,LOW);
+  delay(0.2);
+  SPI.transfer(communication_MSB);
+  SPI.transfer(communication_LSB);
+  delay(0.2);
+  digitalWrite(chipSelectPin,HIGH);
+  delay(0.2);
   SPI.endTransaction();
   Serial.println("SPI communication ended.");
 
@@ -35,3 +39,12 @@ void setup() {
 void loop() {
 
 }
+
+/* Oscilloscope settings:
+ *  channel: serialOutPin or clockPin
+ *  channel 2: chipSelectPin
+ *  trigger: channel 2, mode:edge, slope:positive, sweep:single
+ *  horizontal scale: 20us
+ *  vertical scale: 2V for both channels
+ *  trigger: 108.8 us earlier
+ */
